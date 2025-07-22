a844b989955ea14935d16f524fefc00a9a50e219
Davina Leong <davina.leong.sy@gmail.com>
2025-07-22 13:12:16 +0800

    Added TODO comment for the settings page

---
+++ src/app/editor/settings/page.jsx
@@ -0,0 +1,11 @@
+/**
+ * TODO:
+ *      - Query all Settings
+ *      - Query Setting Enums: LightboxMode, SortOrder, DateFormat
+ *      - Create an Enum selector that opens when the user selects the contenteditable field
+ *      - Settings fields:
+ *          - App Name
+ *          - Lightbox Mode
+ *          - Photo Sort Order
+ *          - Date Format
+ */
