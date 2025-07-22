f846b12ceaab7309c3282799b975daf6f3cec18b
Davina Leong <davina.leong.sy@gmail.com>
2025-07-22 13:05:40 +0800

    Added TODO comments to te photos page

--- src/app/editor/photos/page.jsx
+++ src/app/editor/photos/page.jsx
@@ -1,5 +1,20 @@
 /**
  * TODO:
+ * - Validation
+ *      - Supported formats: jpg, png, gif, webp
+ *      - Max 5 photos at a time
  * - Same style as the editor pages
- * -
+ * - Contenteditable fields (all optional):
+ *      - Title
+ *      - Description
+ *      - Date
+ *      - Gallery Name (type in for now)
+ * - Area to add photos:
+ *      - Mobile
+ *          - Tapping opens the device's photo browser
+ *      - Desktop
+ *          - Allows several methods of uploading photos:
+ *              - Tapping opens the file browser and can select multiple photos.
+ *              - Dragging multiple photos into the area
+ *  - Loader while photos are uploading to cloudinary
  */
